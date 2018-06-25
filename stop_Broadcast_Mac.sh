b634525c5064bac951ff11ee9bb8c7deb0590bef
jason7449@gmail.com <Jinkok_123>
2018-06-25 10:56:39 -0500

    Modify Mac Script

--- stop_Broadcast_Mac.sh
+++ stop_Broadcast_Mac.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-pid=$(lsof -nP -i4TCP:6666 | grep LISTEN |  cut -c 9-12)
+pid=$(lsof -nP -i4TCP:6666 | grep LISTEN |  cut -c 9-13)
 if [[ -n $pid ]]; then
     kill $pid
-    echo "Close port successful!!
+    echo "Close port successfully!!"
     exit
 else
     echo "Does not exist"
